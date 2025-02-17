af011a053d6e56ba630ab1782803a426925bf2b0
Tpavimalini <149188025+Tpavimalini@users.noreply.github.com>
2025-02-17 10:49:38 +0530

    Update litConfig.js

--- src/config/litConfig.js
+++ src/config/litConfig.js
@@ -3,7 +3,7 @@ import { LitNodeClient } from "@lit-protocol/lit-node-client";
 const litClient = new LitNodeClient({
     litNetwork: "serrano", // Use 'cayenne' for mainnet when ready
 });
-
+ 
 export const connectLit = async () => {
     await litClient.connect();
     console.log("✅ Lit Protocol Connected");
